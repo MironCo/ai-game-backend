0a96cfd53fbc3a7da60ac3d21a76a5ab1a479b8b
MironCo <masulicz@gmail.com>
2025-01-04 21:51:59 -0500

    Actually Made It Work *EyeRoll*
    bruh

--- cmd/rd-backend/main.go
+++ cmd/rd-backend/main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Set to release mode, but keep terminal logging
+	gin.SetMode(gin.ReleaseMode)
+
+	// Create router with default logger
 	router := gin.Default()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default fallback
+	}
+
 	router.GET("/", helloWorld)
+
+	fmt.Printf("Server Running On Port 8080\n")
+	router.Run(":" + port)
 }
 
-func helloWorld(*gin.Context) {
-	fmt.Printf("Hello world!\n")
+func helloWorld(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello World!",
+	})
 }
